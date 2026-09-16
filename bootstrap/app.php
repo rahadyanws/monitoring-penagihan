6cd992d4a290b143b4ea98bc0bdcdf2c668aada5
rahadyan <rahadyanws@gmail.com>
2026-09-16 10:00:17 +0700

    setup vercel serverless configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,11 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
+// Tambahkan sebelum return Application::configure...
+if (isset($_ENV['VERCEL'])) {
+    app()->useStoragePath('/tmp/storage');
+}
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
